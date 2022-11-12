24cd820870fa076864c541da8101191ca35c3e1d
bhanu734 <bhanu734@gmail.com>
2022-11-12 02:41:17 -0800

    Initial Commit

---
+++ Animalsapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animalsapp
+//
+//  Created by Mac on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
